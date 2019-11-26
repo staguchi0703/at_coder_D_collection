95ca681435a9245466a5342e43b2b1e3719a80d0
sho Taguchi <s.taguchi0703@gmail.com>
2019-11-27 01:59:08 +0900

    解けた！

--- D138/D138.py
+++ D138/D138.py
@@ -2,11 +2,11 @@
 # VScodeで入力をテキストから読み込んで標準入力に渡す
 import sys
 import os
-f=open(r'.\D138\D138.txt', 'r', encoding="utf-8")
+f = open(r'.\D138\D138.txt', 'r', encoding="utf-8")
 # inputをフルパスで指定
 # win10でファイルを作るとs-jisで保存されるため、読み込みをutf-8へエンコードする必要あり
 # VScodeでinput file開くとutf8になってるんだけど中身は結局s-jisになっているらしい
-sys.stdin=f
+sys.stdin = f
 
 #
 # 入力スニペット
@@ -18,34 +18,43 @@ sys.stdin=f
 # 以下ペースト可
 N, Q = [int(item) for item in input().split()]
 
-tree_list = [input().split() for _ in range(1,N)]
+tree_list = [input().split() for j in range(1, N)]
 
-query_list = [input().split() for _ in range(Q)]
-query_list_int = [[int(k) for k in i]  for i in query_list]
+query_list = [input().split() for k in range(Q)]
+query_list_int = [[int(k) for k in i] for i in query_list]
 
 val_list = [0 for _ in range(N)]
-linkde_node_list = [[] for _ in range(N)]
+
+linked_node_list = [[] for _ in range(N)]
 
 for a, b in tree_list:
-    linkde_node_list[int(a) -1].append(int(b) - 1)
-    linkde_node_list[int(b) -1].append(int(a) - 1)
+    a, b = int(a)-1, int(b) -1
+    linked_node_list[a].append(b)
+    linked_node_list[b].append(a)
 
 
 for index, val in query_list_int:
     val_list[index-1] += val
 
-def dfs(node_index, pairent_index):
-    print('node_index',node_index+1)
-    for child in linkde_node_list[node_index]:
-        print('A',child)
-        if child == pairent_index:
-            continue
-        print('B', child)
-        val_list[child] += val_list[node_index]
-        dfs(child, node_index)
+stack = [0]
+parent = [0] * (N+1)
 
-    return val_list
+#再帰関数を使うとメモリ制限に引っかかるので、whileで順次実行する
+# スタックでLIFOを使う
+# 訪問済みノードを確認し、除外する　=>あるノードの親ノードはXをリストで持っておく
+# この辺データ構造でもてればすっきりしそう
 
-res = dfs(0, -1) 
+while True:
+    v=stack.pop()
+    for child in linked_node_list[v]:
+        if child != parent[v]:
+            parent[child] = v
+            stack.append(child)
+            val_list[child] += val_list[v]
+    if not stack:
+        break
 
-print(*res)
+print(*val_list)
+
+
+# %%
